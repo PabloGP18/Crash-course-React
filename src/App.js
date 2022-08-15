7b6a0d2cadcc519d2333a6d926665499d137ed78
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-16 01:07:00 +0200

    import ParentComponent

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import {Greet} from './components/Greet'
 import {Message} from './components/Message';
 import {ClickHandler} from "./components/ClickHandler";
-
+import {ParentComponent} from "./components/ParentComponent";
 function App() {
   return (
     <div className="App">
-        <Message />
+        <Message /> <br />
         <ClickHandler />
       <Greet name="PGP" age={34} />
         <Greet name="Iron-man" age={58}>
@@ -14,7 +14,8 @@ function App() {
         </Greet>
         <Greet name="Thor" age={42}>
             I'm the God of thunder, and also a children prop!
-        </Greet>
+        </Greet> <br />
+        <ParentComponent />
     </div>
   );
 }
