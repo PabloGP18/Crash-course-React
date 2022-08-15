4e722227bfe6cde800da440b11e671110f3c294a
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-15 22:16:15 +0200

    added a children props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import {Greet} from './components/Greet'
 function App() {
   return (
     <div className="App">
-      <Greet name="PGP"/>
-      <Greet name="Iron-man"/>
-      <Greet name="Thor"/>
+      <Greet name="PGP" age={34} />
+        <Greet name="Iron-man" age={58}>
+            <button>Iron-button</button>
+        </Greet>
+        <Greet name="Thor" age={42}>
+            I'm the God of thunder, and also a children prop!
+        </Greet>
     </div>
   );
 }
