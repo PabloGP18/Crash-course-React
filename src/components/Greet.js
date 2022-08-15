950e3aaa5230c1fcf7436c34ae7c50bfcb537a23
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-15 02:34:22 +0200

    added in comments how to create a function using arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-/*function Greet() {
-    return <h1>Hello PGP</h1>
-}*/
-
-const Greet = () => {
+function Greet() {
     return <h1>Hello PGP</h1>
 }
+
+// this is another way to create a function using arrow.
+/*const Greet = () => {
+    return <h1>Hello PGP</h1>
+}*/
 export default Greet;
