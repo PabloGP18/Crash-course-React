b5d57b5a1c543f83cbf4eb143f442299d781d871
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-16 22:48:41 +0200

    import form in app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {UserGreeting} from "./components/UserGreeting";
 import {NameList} from "./components/NameList";
 import {StyleSheet} from './components/StyleSheet';
 import {Inline} from "./components/Inline";
+import {Form} from "./components/Form";
 
 
 function App() {
@@ -23,15 +24,23 @@ function App() {
         </Greet>
         <Greet name="Thor" age={42}>
             I'm the God of thunder, and also a children prop!
+
         </Greet> <br />
+
+        <Form /> <br />
+
         <ParentComponent /> <br />
+
         <UserGreeting /> <br />
+
         <NameList /> <br />
+
         <StyleSheet />  <br />
         <Inline />  <br />
 
         <h1 className="error">Error</h1>
         <h1 className={styles.success}>Success</h1>
+
     </div>
   );
 }
