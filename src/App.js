07e91a53a74cc33e5cb93cdb7980d467023b0dc1
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-17 00:55:51 +0200

    add on app.js how to use a transition hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import './components/appStyles.css'
 import styles from './components/appStyles.module.css'
+import NAMES from './components/data.json';
+import {startTransition, useState, useTransition} from 'react'
 
 import {Greet} from './components/Greet'
 import {Message} from './components/Message';
@@ -11,10 +13,26 @@ import {NameList} from "./components/NameList";
 import {StyleSheet} from './components/StyleSheet';
 import {Inline} from "./components/Inline";
 import {Form} from "./components/Form";
-
+import {PostList} from "./components/PostList";
+import {PostForm, postForm} from "./components/PostForm";
 
 function App() {
-  return (
+    const [query, setQuery] = useState ('')
+
+    const [inputValue, setInputValue] = useState ('')
+    const [isPending, start] = useTransition()
+
+
+    const changeHandler = (event) => {
+        setInputValue(event.target.value)
+        startTransition(() => setQuery(event.target.value))
+    }
+    const filteredNames = NAMES.filter(item => {
+        return item.first_name.includes(query) || item.last_name.includes(query)
+    })
+
+
+    return (
     <div className="App">
         <Message /> <br />
         <ClickHandler />
@@ -27,7 +45,11 @@ function App() {
 
         </Greet> <br />
 
+        <PostForm /> <br />
+
         <Form /> <br />
+
+        <PostList /> <br />
 
         <ParentComponent /> <br />
 
@@ -41,6 +63,12 @@ function App() {
         <h1 className="error">Error</h1>
         <h1 className={styles.success}>Success</h1>
 
+
+            <input type="text" value={inputValue} onChange={changeHandler} />
+            {isPending && <p>Updating list...</p>}
+             {filteredNames.map((item) => (
+                <p key={item.id}>{item.first_name} {item.last_name}</p>
+            ))}
     </div>
   );
 }
